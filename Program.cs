9bd55b6a9b372369214ea842a6a445ddeb8a56c9
azaharia <alex_zaharia10@yahoo.com>
2021-10-14 10:40:59 +0300

    Add MyClass class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 
-namespace MyFirstApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace MyFirstApp {
+    class MyClass {
+
     }
 }
